e88073c33becdebdd1fb8625b26078869efa317c
Salah <salah.eldeen.mail@gmail.com>
2025-06-30 17:37:30 -0700

    fix middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,24 +1,23 @@
 <?php
 
+use App\Http\Middleware\EnsureCashSessionOpen;
+use App\Http\Middleware\EnsureNoOpenCashSession;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
-            \App\Http\Middleware\EnsureCashSessionOpen::class,
-            \App\Http\Middleware\EnsureNoOpenCashSession::class,
         ]);
-
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
