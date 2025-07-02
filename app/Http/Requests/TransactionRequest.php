db441db7a6fc7eece0ef2576f15f166658f03714
Salah <salah.eldeen.mail@gmail.com>
2025-07-02 17:56:48 +0300

    hot-fix

--- app/Http/Requests/TransactionRequest.php
+++ app/Http/Requests/TransactionRequest.php
@@ -16,7 +16,7 @@ class TransactionRequest extends FormRequest
         return [
             'from_currency_id' => 'required|exists:currencies,id',
             'to_currency_id' => 'required|exists:currencies,id',
-            'amount_original' => 'required|numeric|min:0.01',
+            'original_amount' => 'required|numeric|min:0.01',
         ];
     }
 }
