ce6150c67daaa5c3bf4c9be78210a98b20268eaa
Salah <salah.eldeen.mail@gmail.com>
2025-07-05 03:04:07 +0300

    enhancing the code stuctrue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,9 @@
 
 namespace App\Providers;
 
-use App\Models\CashSession;
 use App\Models\Transaction;
 use App\Policies\TransactionPolicy;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
@@ -25,13 +25,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Model::automaticallyEagerLoadRelationships();
+
         Inertia::share([
             'auth' => fn () => [
                 'user' => Auth::user(),
                 'roles' => Auth::check() ? Auth::user()->roles->pluck('name') : [],
                 // 'permissions' => Auth::check() ? Auth::user()->allPermissions()->pluck('name') : [],
             ],
-            'cash_session' => fn () => CashSession::whereIn('status', ['active', 'pending'])->latest()->first() ?? false,
         ]);
 
         Gate::policy(Transaction::class, TransactionPolicy::class);
