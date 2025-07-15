cff1057858dd61942d48354c7e16ba6470536be2
Salah <salah.eldeen.mail@gmail.com>
2025-07-15 17:34:18 +0300

    hot fix

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -50,7 +50,7 @@ class TransactionController extends Controller
     public function completeStatus(Transaction $transaction)
     {
         try {
-            if ($this->transactionService->getCurrencyAvailableBalance($transaction->to_currency_id) <= $transaction->converted_amount) {
+            if ($this->transactionService->getCurrencyAvailableBalance($transaction->to_currency_id) < $transaction->converted_amount) {
                 return $this->failed('الرصيد غير كافٍ لإتمام المعاملة.');
             }
             $this->transactionService->confirmCashMovement($transaction);
